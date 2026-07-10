1b7734838ce15d742c0fc1a44c4ce22c89099917
shunyi <shunyi@example.com>
2026-07-10 12:50:37 +0800

    setup AI project environment

--- main.py
+++ main.py
@@ -6,9 +6,26 @@ name = input("请输入你的名字：")
 
 print(f"你好，{name}！")
 
-question = input("请输入一句话：")
+while True:
+    question = input("\n你：").strip()
 
-print()
-print("你输入的内容长度是：", len(question))   # 新增这一行
-print("你刚刚输入的是：")
-print(question)
+    if question in ["exit", "quit", "退出"]:
+        print("AI：再见！")
+        break
+
+    if question == "":
+        print("AI：请重新输入！")
+        continue
+
+    if question == "help":
+        print("======== 帮助 ========")
+        print("exit   退出程序")
+        print("quit   退出程序")
+        print("help   查看帮助")
+        print("======================")
+        continue
+
+
+    print("AI：")
+    print("你刚刚说的是：")
+    print(question)
